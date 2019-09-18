27b93c3f2423bbfa6226de0d46c645fd48be7f36
Unknown <az2k@verizon.net>
2019-09-17 20:33:29 -0400

    Add fllipit and misc changes

---
+++ db_converter.py
@@ -0,0 +1,93 @@
+import sqlite3 as sql
+import time
+
+scoring = {
+    8: 20,
+    9: 15,
+    10: 20,
+    11: 15,
+    12: 15,
+    13: 10,
+    14: 10,
+    15: 10,
+    16: 15,
+    17: 10,
+    18: 10,
+    20: 10,
+    21: 20,
+    23: 10,
+    24: 5,
+    25: 10
+}
+scoring_extra = {
+    "M08a": {
+        0: 0,
+        1: 15,
+        2: 20
+    },
+    "M11a": {
+        0: 0,
+        1: 10,
+        2: 15
+    },
+    "M14a": {
+        0: 0,
+        1: 5,
+        2: 10,
+        3: 20,
+        4: 30,
+        5: 45,
+        6: 60
+    }
+}
+
+while True:
+    time.sleep(15)
+    print("Refreshed at", time.ctime())
+
+    conn = sql.connect('data_2019qualifier.db')
+    conn2 = sql.connect('fllipit\\fllipit.db')
+    team_conn = sql.connect('teams.db')
+    cur = conn.cursor()
+    cur2 = conn2.cursor()
+    team_cur = team_conn.cursor()
+    team_cur.execute("SELECT * FROM master_teams order by team")
+    teamscores=[]
+    teamdata = team_cur.fetchall()
+
+    for i in range (0,len(teamdata)):
+        teamscores.append([0,'','',0,0,0,0,0,0,0,0,0,0])
+
+    for i in range(0,len(teamdata)):
+        cur.execute("SELECT * FROM scout WHERE team=? ORDER BY match",(teamdata[i][0],))
+        count = 3
+        matchdata = cur.fetchall()
+        teamscores[i][0]=teamdata[i][0]
+        teamscores[i][1]=teamdata[i][1]
+        teamscores[i][2]=teamdata[i][2]
+        for z in range(0,len(matchdata)):
+            matchscore = 0
+            for key, score in scoring.items():
+                matchscore += matchdata[z][key] * score
+            matchscore += scoring_extra["M08a"][matchdata[z][19]]
+            matchscore += scoring_extra["M11a"][matchdata[z][22]]
+            matchscore += scoring_extra["M14a"][matchdata[z][26]]
+            teamscores[i][count]=matchscore
+            teamscores[i][count+1] = 6 - matchdata[z][26]
+            count = count + 2
+
+
+    cur2.execute("DELETE FROM team")
+    conn2.commit()
+    for i in range(0,len(teamscores)):
+        insertCommand = """INSERT INTO 'team' ('number','name','affiliation', 'round1', 'round1penalties', 'round2', 'round2penalties', 'round3', 'round3penalties', 'round4', 'round4penalties', 'round5', 'round5penalties') VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+        insert_data = ()
+        for f in range(0, 13):
+            insert_data += (teamscores[i][f],)
+        cur2.execute(insertCommand, insert_data)
+
+
+    conn2.commit()
+    conn.close()
+    conn2.close()
+    team_conn.close()
